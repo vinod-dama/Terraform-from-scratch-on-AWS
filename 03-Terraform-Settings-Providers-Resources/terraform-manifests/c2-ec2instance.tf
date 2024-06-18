7d8cc24f70b55ea4009a392769ced8debf5fc678
Vinod Dama <vinod.dama@gmail.com>
2024-06-18 14:58:12 +0530

    providers and resource types in terraform

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,13 +1,4 @@
 # Resource: EC2 Instance
-/*resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
-  instance_type = "t3.micro"
-  user_data = file("${path.module}/app1-install.sh")
-  tags = {
-    "Name" = "EC2 Demo"
-  }
-}
-*/
 
 resource "aws_instance" "myec2-vm" {
   ami = "ami-0440d3b780d96b29d"
