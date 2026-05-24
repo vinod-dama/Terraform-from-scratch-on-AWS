0baa1f6168e5bbcbb54b70307935dc8277be0c1c
Vinod Dama <vinod.dama@gmail.com>
2026-05-24 08:59:43 +0530

    adding profile details if not default

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -16,5 +16,11 @@ provider "aws" {
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
 $HOME/.aws/credentials
+
+else if you need to use a different profile, follow below
+provider "aws" {
+  region = "us-east-1"
+  profile = "<profilename>" #profilename to be picked from $HOME/.aws/credentials file 
+}
 */
 
