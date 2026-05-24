d6d5dc60c8b9d564a30dfd4ccf568403098b4c86
Vinod Dama <vinod.dama@gmail.com>
2026-05-24 09:25:35 +0530

    ami-id update

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,7 +1,7 @@
 # Resource: EC2 Instance
 
 resource "aws_instance" "myec2-vm" {
-  ami = "ami-0440d3b780d96b29d"
+  ami = "ami-09ed39e30153c3bf9"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
