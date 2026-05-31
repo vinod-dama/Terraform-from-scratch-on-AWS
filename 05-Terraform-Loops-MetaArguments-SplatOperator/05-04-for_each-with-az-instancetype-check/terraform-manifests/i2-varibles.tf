b81c85a49406d5d28e1345e1c9fac23194d46424
Vinod Dama <vinod.dama@gmail.com>
2026-05-31 06:49:10 +0530

    Privsion Ec2 instances in regions that support instance_types with dynamic Ami-Id & launch the website

---
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-04-for_each-with-az-instancetype-check/terraform-manifests/i2-varibles.tf
@@ -0,0 +1,34 @@
+variable "region" {
+  description = "aws region for resource"
+  default = "us-east-1"
+  type = string
+}
+
+variable "instance_type" {
+    description = "ec2 instance type"
+    default = "t3.micro"
+    type = string
+
+}
+
+
+variable "key_name" {
+    description = "key pair to connect to ec2"
+    default = "vinod-keypair-aws"
+    type = string
+}   
+
+variable "instance_type_list" {
+    description = "list of ec2 instances"
+    default = ["t3.micro", "t3.small",  "c7i-flex.large"]
+    type = list(string)
+}
+
+variable "instance_type_map" {
+    description = "map of ec2 instance"
+    default = {
+        "dev" = "t3.micro",
+        "qa" = "t3.small",
+        "prod" = "c7i-flex.large"
+    }  
+}
