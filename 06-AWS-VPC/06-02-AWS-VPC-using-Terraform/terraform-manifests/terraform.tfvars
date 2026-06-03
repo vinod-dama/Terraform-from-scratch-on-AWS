1ed2ca7077912a13870117a280a6ede0f6f7e436
Vinod Dama <vinod.dama@gmail.com>
2026-06-03 23:22:09 +0530

    Launching VPC using terraform

--- 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-instance_type = "t3.micro"
-region = "ap-south-1"
-key_name = "vinod-keypair-aws"
-business_divsion = "AI"
--- 06-AWS-VPC/06-03-v2-vpc-module-standardized/terraform-manifests/i2-generic-variables.tf
+++ 06-AWS-VPC/06-03-v2-vpc-module-standardized/terraform-manifests/i2-generic-variables.tf
@@ -3,21 +3,21 @@
 
 variable "region" {
   description = "Region of AWS to provision the resources"
-  default = "ap-south-1"
-  type = string
-  
-}
-
-variable "department" {
-  description = "department"
-  default = "AI"
+  default = "us-west-2"
+  #default = "ap-south-1"
   type = string
   
 }
 
 variable "LOB" {
-  description = "LOB"
-  default = "Tech"
   type = string
+  default = "AI"
   
 }
+
+variable "department" {
+  type = string
+  default = "devops"
+  
+}
+
