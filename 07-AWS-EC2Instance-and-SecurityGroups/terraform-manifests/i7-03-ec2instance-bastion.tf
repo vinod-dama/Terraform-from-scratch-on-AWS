9a3e81751c89677efc087a6d1d0fb3ee3f3f6f7b
Vinod Dama <vinod.dama@gmail.com>
2026-06-12 22:11:48 +0530

    add ec2, null resource, eip

--- 07-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/i7-03-ec2instance-bastion.tf
+++ 07-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/i7-03-ec2instance-bastion.tf
@@ -1,13 +1,16 @@
+# AWS EC2 Instance Terraform Module
+# Bastion Host - EC2 Instance that will be created in VPC Public Subnet
+
 module "bastion_ec2" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "6.4.0"
-  name = "bastion-instance"
-
-  ami = data.aws_ami.ami_linux.id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = module.vpc.public_subnets[0]
+  # insert the 10 required variables here
+  name                   = "${local.name}-BastionHost"
+  #instance_count         = 5
+  ami                    = data.aws_ami.ami_linux.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
-
   tags = local.common_tags
 }
