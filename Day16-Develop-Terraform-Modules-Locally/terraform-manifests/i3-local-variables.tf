db1e09876acc8e1b05a23fcc40f942dcd442ed25
Vinod Dama <vinod.dama@gmail.com>
2026-06-24 11:43:18 +0530

    Formating standards

--- Day16-Develop-Terraform-Modules-Locally/terraform-manifests/i3-local-variables.tf
+++ Day16-Develop-Terraform-Modules-Locally/terraform-manifests/i3-local-variables.tf
@@ -1,11 +1,11 @@
 # Define Local Values in Terraform
 locals {
-    department = var.department
-    lob = var.LOB
-    name = "${var.department}-${var.LOB}"
+    department  = var.department
+    lob         = var.LOB
+    name        = "${var.department}-${var.LOB}"
 
     common_tags = {
-        owners = local.department
+        owners      = local.department
         environment = local.lob
     }
 }
--- Day16-Develop-Terraform-Modules-Locally/terraform-manifests/i4-01-vpc.tf
+++ Day16-Develop-Terraform-Modules-Locally/terraform-manifests/i4-01-vpc.tf
@@ -1,9 +1,9 @@
 #VPC Module Block
 module "vpc" {
-  source  = "./modules/terraform-aws-vpc-master"
+  source           = "./modules/terraform-aws-vpc-master"
 
-  name = "${local.name}-myvpc"
-  cidr = var.vpc_cidr
+  name             = "${local.name}-myvpc"
+  cidr             = var.vpc_cidr
 
   azs              = var.azs
   private_subnets  = var.vpc_private_subnets
@@ -11,16 +11,16 @@ module "vpc" {
   database_subnets = var.vpc_db_subnets
 
   # NAT Gateways - Outbound Communication
-  enable_nat_gateway = var.enable_nat_gateway
-  single_nat_gateway = var.single_nat_gateway
+  enable_nat_gateway     = var.enable_nat_gateway
+  single_nat_gateway     = var.single_nat_gateway
   one_nat_gateway_per_az = var.one_nat_gateway_per_az
 
   #create_database_subnet_group = true
   #create_database_subnet_route_table= true
   
    # VPC DNS Parameters
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_hostnames              = true
+  enable_dns_support                = true
 
   create_database_nat_gateway_route = false
 
