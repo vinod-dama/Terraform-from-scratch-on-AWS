2b9a452ac9f6f4a56d3f670f908a56679180cfff
Vinod Dama <vinod.dama@gmail.com>
2026-08-29 10:31:45 +0530

    README and file formatting updates

--- Day10-ALB-Path-Based-Routing/terraform-manifests/i5-05-securitygroup-loadbalancersg.tf
+++ Day10-ALB-Path-Based-Routing/terraform-manifests/i5-05-securitygroup-loadbalancersg.tf
@@ -1,16 +1,16 @@
 # AWS EC2 Security Group Terraform Module
 # Security Group for Public Bastion Host
 module "lb_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.1"
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "5.3.1"
 
-  name = "lb-sg"
-  description = "Security Group with SSH port open for everybody (IPv4 CIDR), egress ports are all world open"
-  vpc_id = module.vpc.vpc_id
+  name                = "lb-sg"
+  description         = "Security Group with SSH port open for everybody (IPv4 CIDR), egress ports are all world open"
+  vpc_id              = module.vpc.vpc_id
   # Ingress Rules & CIDR Blocks
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   # Egress Rule - all-all open
-  egress_rules = ["all-all"]
+  egress_rules        = ["all-all"]
   tags = local.common_tags
 }
--- Day10-ALB-Path-Based-Routing/terraform-manifests/i8-elasticip.tf
+++ Day10-ALB-Path-Based-Routing/terraform-manifests/i8-elasticip.tf
@@ -2,15 +2,15 @@
 # Resource - depends_on Meta-Argument
 resource "aws_eip" "bastion_eip" {
   depends_on = [module.bastion_ec2, module.vpc]
-  tags = local.common_tags
-  instance = module.bastion_ec2.id
-  domain = "vpc"  
+  tags       = local.common_tags
+  instance   = module.bastion_ec2.id
+  domain     = "vpc"  
   
 ## Local Exec Provisioner:  local-exec provisioner (Destroy-Time Provisioner - Triggered during deletion of Resource)
   provisioner "local-exec" {
-    command = "echo Destroy time prov `date` >> destroy-time-prov.txt"
+    command     = "echo Destroy time prov `date` >> destroy-time-prov.txt"
     working_dir = "local-exec-output-files/"
-    when = destroy
+    when        = destroy
     #on_failure = continue
   }  
 }
