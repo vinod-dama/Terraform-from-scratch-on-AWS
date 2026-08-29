2bc6cfdbc007fd08e481ccdf3439c96d5b73b927
Vinod Dama <vinod.dama@gmail.com>
2026-08-29 15:06:21 +0530

    File format edits

--- Day10-ALB-Path-Based-Routing/terraform-manifests/i3-local-variables.tf
+++ Day10-ALB-Path-Based-Routing/terraform-manifests/i3-local-variables.tf
@@ -1,11 +1,11 @@
 # Define Local Values in Terraform
 locals {
     department = var.department
-    lob = var.LOB
+    lob  = var.LOB
     name = "${var.department}-${var.LOB}"
 
     common_tags = {
-        owners = local.department
+        owners      = local.department
         environment = local.lob
     }
 }
